1d06d934ec566fcd64585eef267b40c1fa203d5b
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2023-10-26 23:13:12 +0530

    feat: added login logout feature

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/sidebar";
 import Header from "@/components/header";
+import NextAuthProvider from "@/auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,19 +17,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <GlobalState>
-          <div className="flex h-screen overflow-hidden">
-            <Sidebar />
-            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-              <Header />
-              <main>
-                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                  {children}
-                </div>
-              </main>
+        <NextAuthProvider>
+          <GlobalState>
+            <div className="flex h-screen overflow-hidden">
+              <Sidebar />
+
+              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                <Header />
+                <main>
+                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                    {children}
+                  </div>
+                </main>
+              </div>
             </div>
-          </div>
-        </GlobalState>
+          </GlobalState>
+        </NextAuthProvider>
       </body>
     </html>
   );
