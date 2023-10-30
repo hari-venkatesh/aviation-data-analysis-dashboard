6273c5801a8686e94c2195857a03bc77b4d9a360
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2023-10-30 09:01:51 +0530

    feat: added dashboard layout and cards

---
+++ src/app/page.js
@@ -0,0 +1,39 @@
+import DashboardLayout from "@/components/dashboard/page";
+
+//get all airports data
+
+async function extractAllAirports() {
+  const res = await fetch("http://localhost:3000/api/airports/all-airports", {
+    method: "GET",
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  return data;
+}
+
+//get all airlines data
+
+async function extractAllAirlines() {
+  const res = await fetch("http://localhost:3000/api/airlines/all-airlines", {
+    method: "GET",
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  return data;
+}
+
+export default async function Home() {
+  const allAirports = await extractAllAirports();
+  const allAirlines = await extractAllAirlines();
+
+  return (
+    <DashboardLayout
+      allAirports={allAirports && allAirports.data}
+      allAirlines={allAirlines && allAirlines.data}
+    />
+  );
+}
