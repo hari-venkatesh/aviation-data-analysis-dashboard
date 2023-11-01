bd8f64fb74aa36acc75e6420d2c044c38316de15
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2023-11-01 11:55:08 +0530

    fix: key prop fix

--- src/components/model/index.js
+++ src/components/model/index.js
@@ -21,40 +21,42 @@ export default function Modal({
                 <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
                   <h3 className="text-3xl font-semibold">{title}</h3>
                 </div>
-                <div key={formControls.name} className="relative p-5 flex-auto flex flex-col gap-5">
-                  {formControls && formControls.length
-                    ? formControls.map((item) =>
-                        item.componentType === "input" ? (
-                          <Input
-                            type={item.type}
-                            placeholder={item.placeholder}
-                            label={item.label}
-                            value={formData && formData[item.id]}
-                            onChange={(e) =>
-                              setFormData({
-                                ...formData,
-                                [item.id]:
-                                  item.type === "number"
-                                    ? parseInt(e.target.value)
-                                    : e.target.value,
-                              })
-                            }
-                          />
-                        ) : item.componentType === "select" ? (
-                          <Select
-                            value={formData && formData[item.id]}
-                            onChange={(e) =>
-                              setFormData({
-                                ...formData,
-                                [item.id]: e.target.value,
-                              })
-                            }
-                            label={item.label}
-                            options={item.options}
-                          />
-                        ) : null
-                      )
-                    : null}
+                <div key={formControls.name}>
+                  <div className="relative p-5 flex-auto flex flex-col gap-5">
+                    {formControls && formControls.length
+                      ? formControls.map((item) =>
+                          item.componentType === "input" ? (
+                            <Input
+                              type={item.type}
+                              placeholder={item.placeholder}
+                              label={item.label}
+                              value={formData && formData[item.id]}
+                              onChange={(e) =>
+                                setFormData({
+                                  ...formData,
+                                  [item.id]:
+                                    item.type === "number"
+                                      ? parseInt(e.target.value)
+                                      : e.target.value,
+                                })
+                              }
+                            />
+                          ) : item.componentType === "select" ? (
+                            <Select
+                              value={formData && formData[item.id]}
+                              onChange={(e) =>
+                                setFormData({
+                                  ...formData,
+                                  [item.id]: e.target.value,
+                                })
+                              }
+                              label={item.label}
+                              options={item.options}
+                            />
+                          ) : null
+                        )
+                      : null}
+                  </div>
                 </div>
                 <div className="flex gap-2 items-center justify-end p-6 border-t border-solid rounded-b">
                   <Button text={"Add"} onClick={onAdd} />
